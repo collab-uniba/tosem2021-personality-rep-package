ed97849b7a0fca54a3be85248b302a08ba4efee5
Fabio Calefato <fabio.calefato@uniba.it>
2021-03-11 09:04:19 +0100

    Rename dataprep.sh to setup.sh

--- dataprep.sh
+++
@@ -1,5 +0,0 @@
-
-wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip
-
-unzip wiki-news-300d-1M.vec.zip -d twitpersonality/FastText
-mv twitpersonality/FastText/wiki-news-300d-1M.vec twitpersonality/FastText/dataset.vec
