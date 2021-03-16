0cc433a0ca06faa6f0724e0fcdb839656f0995c2
Fabio Calefato <fcalefato@gmail.com>
2021-03-16 17:56:12 +0100

    Add violins plot to phase1 analyses

--- src/analysis/phase1_analysis.py
+++ src/analysis/phase1_analysis.py
@@ -1,5 +1,9 @@
-import pandas as pd
 import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 from analysis import test_normal_distribution as normal
 
 
@@ -65,15 +69,64 @@ def pairwise_correlations(df_o, df_c, df_e, df_a, df_n):
         json.dump(corr_matrices, js_f, indent=4)
 
 
+def mailcorpus_stats():
+    emails_dict = dict()
+    no_emails = 0
+    all_bodies = ""
+    words_per_email = dict()
+    tot_words_user = dict()
+    with open(file="dataset/goldstandard/mailcorpus-sha.json", mode="r") as jsf:
+        emails = json.load(jsf)
+        # count emails
+        for addr in emails:
+            no_emails += len(emails[addr])
+            bodies = ' '.join(emails[addr])
+            emails_dict[addr] = bodies
+            all_bodies += " " + bodies
+            tot_words_user[addr] = len(bodies.split())
+            words_per_email[addr] = tot_words_user[addr] / len(emails[addr])
+    # count addresses
+    with open(file="dataset/goldstandard/address_list_sha.txt", mode="r") as f:
+        no_subjects = len(f.readlines())
+
+    with open(file="results/phase1/descriptive_stats.txt", mode="w") as f:
+        f.write("No. of subjects: {}\n".format(no_subjects))
+        f.write("Total no. of emails: {}\n".format(no_emails))
+        f.write("Total no. of words: {}\n".format(len(all_bodies.split())))
+        f.write("Avg. no. of emails per user: {:.0f}\n".format(no_emails / no_subjects))
+        f.write("Avg. no. of words per email: {:.0f}\n".format(np.mean(list(words_per_email.values()))))
+        f.write("Avg. no. words per user: {:.0f}\n".format(np.mean(list(tot_words_user.values()))))
+
+
+def groundtruth_stats(email_addresses):
+    with open(file="dataset/goldstandard/ipip-scores-sha.json", mode="r") as jsf:
+        df = pd.DataFrame.from_dict(json.load(jsf))
+        df = df.loc[df['email'].isin(email_addresses)]
+        openness = df['openness'].values
+        conscientiousness = df['conscientiousness'].values
+        extraversion = df['extraversion'].values
+        agreeableness = df['agreeableness'].values
+        neuroticism = df['neuroticism'].values
+
+        fig, axes = plt.subplots()
+        axes.violinplot(dataset=[openness, conscientiousness, extraversion, agreeableness, neuroticism], showmeans=True)
+        xticklabels = ['Ope', 'Con', 'Ext', 'Agr', 'Neu']
+        axes.set_xticks([1, 2, 3, 4, 5])
+        axes.set_xticklabels(xticklabels)
+        axes.yaxis.grid(True)
+        plt.savefig("results/phase1/violins.png")
+
+
 if __name__ == '__main__':
-    tools = {'LIWC': 'dataset/LIWC/results/results.json',
-             'PR': 'dataset/PersonalityRecognizer/results/results.json'}  # ,
-    # 'TP': 'dataset/twitpersonality/Results/results.json',
-    # 'PI': 'dataset/PersonalityInsights/results'}
-    df_o, df_c, df_e, df_a, df_n = build_dataframe(tools)
+    with open(file="dataset/goldstandard/address_list_sha.txt", mode="r") as f:
+        emails_addr = [e.strip() for e in f.readlines()]
+        groundtruth_stats(emails_addr)
+        mailcorpus_stats()
 
-    normality_test(df_o, df_c, df_e, df_a, df_n, tools.keys())
-    pairwise_correlations(df_o, df_c, df_e, df_a, df_n)
-
-
-
+        tools = {'LIWC': 'dataset/LIWC/results/results.json',
+                 'PR': 'dataset/PersonalityRecognizer/results/results.json'}  # ,
+        # 'TP': 'dataset/twitpersonality/Results/results.json',
+        # 'PI': 'dataset/PersonalityInsights/results'}
+        df_o, df_c, df_e, df_a, df_n = build_dataframe(tools)
+        normality_test(df_o, df_c, df_e, df_a, df_n, tools.keys())
+        pairwise_correlations(df_o, df_c, df_e, df_a, df_n)
