d6d9a9dfa3ae50ce7aaa4a6485a29fba97f3b869
Fabio Calefato <fcalefato@gmail.com>
2021-03-17 18:08:43 +0100

    Add instruction to invoke TwitPersonality execution

--- tools_execution.sh
+++ tools_execution.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=./src
+PYTHONPATH=.:./src
 
 echo "Running tools"
 
@@ -9,21 +9,21 @@ cd PersonalityRecognizer || exit
 source PersonalityRecognizer.sh -i ../dataset/PersonalityRecognizer/data -d -t 2 -m 4 > output.txt
 mv output.txt ../dataset/PersonalityRecognizer/results/
 cd ..
-python src/pr_score_transformation.py
+python src/pr.py
 
 echo "LIWC"
 echo "LIWC is a desktop app, execute it manually to create LIWC2007_output.csv stored in dataset/LIWC/data"
 # if you used LIWC 2015, pass 2015 as parameter
-python src/liwc_scores_transformation.py 2007
+python src/liwc.py 2007
 
 
 echo "TwitPersonality"
-
+python src/tp.py
 
 echo "Personality Insights"
 
 
 echo "Computing PHASE 1 analyses"
-python src/analysis/run_analysis.py
+python src/analysis/phase1_analysis.py
 
 echo "Done"
