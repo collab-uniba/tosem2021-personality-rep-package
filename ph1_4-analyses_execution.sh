4572a79794f633031957b7b3ba1d0d16a8206a5e
Fabio Calefato <fcalefato@gmail.com>
2021-03-21 07:46:54 +0100

    Add boilerplate preamble to stop script on any first error

--- ph1_4-analyses_execution.sh
+++ ph1_4-analyses_execution.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# -Ee ensures script will stop on first command failure (alternatively, use: set -o errexit)
+# -u ensures script will exit on first unset variable encountered (alternatively, use: set -o nounset)
+# -o pipefail ensures that if any command in a set of piped commands failed, the overall status is that of the failed one
+set -Eeuxo pipefail
 export PYTHONPATH=.:./src
 
 echo "Computing PHASE 1 analyses"
