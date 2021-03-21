d2b6efb23e0588899bf04dde75980873ba968537
Fabio Calefato <fcalefato@gmail.com>
2021-03-21 08:18:12 +0100

    Remove debug x param

--- ph1_3-tools_execution.sh
+++ ph1_3-tools_execution.sh
@@ -3,7 +3,7 @@
 # -Ee ensures script will stop on first command failure (alternatively, use: set -o errexit)
 # -u ensures script will exit on first unset variable encountered (alternatively, use: set -o nounset)
 # -o pipefail ensures that if any command in a set of piped commands failed, the overall status is that of the failed one
-set -Eeuxo pipefail
+set -Eeuo pipefail
 export PYTHONPATH=.:./src:./twitpersonality
 
 echo "Running tools"
