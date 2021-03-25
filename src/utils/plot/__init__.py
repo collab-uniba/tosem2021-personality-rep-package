2203e6e89769ef8834b99e36ec835fdfbbb64a6f
Fabio Calefato <fcalefato@gmail.com>
2021-03-25 13:40:15 +0100

    Adjust plot y ticks

--- src/utils/plot/__init__.py
+++ src/utils/plot/__init__.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def save_violins_plot(openness, conscientiousness, extraversion, agreeableness, neuroticism, path):
@@ -7,5 +8,6 @@ def save_violins_plot(openness, conscientiousness, extraversion, agreeableness, 
     xticklabels = ['Ope', 'Con', 'Ext', 'Agr', 'Neu']
     axes.set_xticks([1, 2, 3, 4, 5])
     axes.set_xticklabels(xticklabels)
+    axes.set_yticks(np.arange(1, 5.5, step=0.5))  # Set label locations.
     axes.yaxis.grid(True)
     plt.savefig(path)
