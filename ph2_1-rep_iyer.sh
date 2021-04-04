91b2095d5952127e81bd8532243eab9e3ac8152c
Fabio Calefato <fcalefato@gmail.com>
2021-04-04 12:16:34 +0200

    Add test option as a parameter

--- ph2_1-rep_iyer.sh
+++ ph2_1-rep_iyer.sh
@@ -5,8 +5,8 @@
 # -o pipefail ensures that if any command in a set of piped commands failed, the overall status is that of the failed one
 set -Eeuo pipefail
 
-# remove parameter `test` to execute the scripts of the full dataset
-Rscript --vanilla rep-packages/iyer2019tse/RQ0/RQ0.R  rep-packages/iyer2019tse/data/final.csv test
-Rscript --vanilla rep-packages/iyer2019tse/RQ1/RQ1.R  rep-packages/iyer2019tse/data/final_LIWC.csv test
-Rscript --vanilla rep-packages/iyer2019tse/RQ2/RQ2.R  rep-packages/iyer2019tse/data/final_LIWC.csv test
-Rscript --vanilla rep-packages/iyer2019tse/RQ3/RQ3.R  rep-packages/iyer2019tse/data/final_LIWC.csv test
+# pass the parameter `test` to execute the scripts of a random subsample of the full dataset
+Rscript --vanilla rep-packages/iyer2019tse/RQ0/RQ0.R  rep-packages/iyer2019tse/data/final.csv "$1"
+Rscript --vanilla rep-packages/iyer2019tse/RQ1/RQ1.R  rep-packages/iyer2019tse/data/final_LIWC.csv "$1"
+Rscript --vanilla rep-packages/iyer2019tse/RQ2/RQ2.R  rep-packages/iyer2019tse/data/final_LIWC.csv "$1"
+Rscript --vanilla rep-packages/iyer2019tse/RQ3/RQ3.R  rep-packages/iyer2019tse/data/final_LIWC.csv "$1"
