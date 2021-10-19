4b0326285c7636b343e15107b656c95acf6cb5e6
Fabio Calefato <fabio.calefato@uniba.it>
2021-10-19 17:53:25 +0200

    Update PersonalityRecognizer.bat

--- PersonalityRecognizer/PersonalityRecognizer.bat
+++ PersonalityRecognizer/PersonalityRecognizer.bat
@@ -4,7 +4,7 @@ rem WINDOWS LAUNCH SCRIPT
 rem ENVIRONMENT VARIABLES TO MODIFY
 
 set JDK_PATH="C:\Program Files\Java\jdk1.6.0_01"
-set WEKA="H:\apps\weka-3-4\weka.jar"
+set WEKA="weka-3-4\weka.jar"
 
 rem ----------------------------------
 
